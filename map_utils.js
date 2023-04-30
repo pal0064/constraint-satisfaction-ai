0fd897b887c697c890878927422713541867390a
pal0064 <apal@arizona.edu>
2023-04-29 18:04:40 -0700

    new changes

--- map_utils.js
+++ map_utils.js
@@ -1,12 +1,15 @@
-function getInitials(stateName){
-    var initials = stateName
-      .split(" ")
-      .map(function(word) { return word.charAt(0); })
-      .join("");
-      return initials
-    }
+var SKIP_STATES  = [
+              "American Samoa", "Guam", "Commonwealth of the Northern Mariana Islands","Puerto Rico","United States Virgin Islands",
+              "Australian Capital Territory",
+              "Daman & Diu",
+              "Lakshadweep",
+              "Chandigarh",
+              "Puducherry",
+              "Dadara & Nagar Havelli",
+              "NCT of Delhi"
+            ]
 
-    function getGeoDetails(country){
+function getGeoDetails(country){
         switch(country) {
                 case 'australia':
                 var projection = d3.geoMercator()
@@ -14,35 +17,35 @@ function getInitials(stateName){
                                 .scale(1000);
                 var topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/australia_states.json";
                   break;
-                  // case 'us':
-                  //     var projection = d3.geoAlbersUsa()
-                  //         .scale(1200)
-                  //         .translate([width / 2, height / 2]);
+                  case 'us':
+                      var projection = d3.geoAlbersUsa()
+                          .scale(1200)
+                          .translate([width / 2, height / 2]);
 
-                  //         var topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/usa_states.json"
-                  //     break;
-//                       case 'japan':
-//                           var projection = d3.geoMercator()
-//                               .center([139.6503, 35.6762])
-//                               .scale(1300)
-//                               .translate([width / 2, height / 2]);
-//                           var  topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/japan_states.json"
-//                         break;
-//                 case 'india':
-//                   var projection = d3.geoMercator()
-//     .center([78.9629, 22.5937])
-//     .scale(1000)
-//     .translate([width / 2, height / 2]);
-// var  topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/india_states.json"
-// break;
+                          var topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/usa_states.json"
+                      break;
+                      case 'japan':
+                          var projection = d3.geoMercator()
+                              .center([139.6503, 35.6762])
+                              .scale(1300)
+                              .translate([width / 2, height / 2]);
+                          var  topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/japan_states.json"
+                        break;
+                case 'india':
+                  var projection = d3.geoMercator()
+    .center([78.9629, 22.5937])
+    .scale(1000)
+    .translate([width / 2, height / 2]);
+var  topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/india_states.json"
+break;
 
-//               case 'china':
-// var projection = d3.geoMercator()
-//     .center([105, 35])
-//     .scale(800)
-//     .translate([width / 2, height / 2]);
-//     var  topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/china_states.json"
-//     break;
+case 'china':
+var projection = d3.geoMercator()
+    .center([105, 35])
+    .scale(800)
+    .translate([width / 2, height / 2]);
+    var  topoJsonURL = "https://raw.githubusercontent.com/pal0064/maps/master/topojsons/china_states.json"
+    break;
 
               default:
                 var projection = d3.geoMercator()
@@ -69,22 +72,21 @@ function createMap(svg){
 var data = topojson.feature(topo, topo.objects.states);
 data.features = data.features.filter(function(d) {
   // console.log(data.features)
-return d.properties.STATE_NAME !== "Australian Capital Territory" &&
-       d.properties.STATE_NAME !== "Tasmania" &&
-       d.properties.STATE_NAME !== "";
+return !SKIP_STATES.includes(d.properties.STATE_NAME) && 
+  d.properties.STATE_NAME !== "" ;
 });
       svg.append("g")
         .selectAll("path")
         .data(data.features)
         .enter()
         .append("path")
-        // .transition()
-        // .ease(d3.easePoly)
+    //     .transition()
+    //     .ease(d3.easePoly)
     // .duration(500)
     // .delay(function(d, i) { return i * 500; }) // Add delay for each feature
     .attr("d", path)
-    
-        .style("fill", function(d,i) {
+    .attr("id", function(d) { return d.properties.STATE_NAME; })
+    .style("fill", function(d,i) {
             // Color the map based on CSP algorithm
             // Here you can write your own CSP algorithm based on your data
             return colorScale(i);
@@ -102,7 +104,7 @@ return d.properties.STATE_NAME !== "Australian Capital Territory" &&
             .attr("y", function(d) { return path.centroid(d)[1]; })
             .attr("text-anchor", "middle")
             .attr("alignment-baseline", "central")
-            .style("font-size", "15px");
+            .style("font-size", "10px");
     
     });
     
