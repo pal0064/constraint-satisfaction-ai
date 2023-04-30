e2b4777598e4a796a08d94e5e59184f9f3e9abb7
pal0064 <apal@arizona.edu>
2023-04-30 03:53:46 -0700

    formatted code

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Define the size of the SVG element
 var width = 960,
     height = 600;
-    
+
 // Create the SVG element
 var svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    .attr("id","datavizMap");
+    .attr("id", "datavizMap");
 
 var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-var allColors = ["red", "green", "blue", "yellow","purple","orange","pink"];   
+var allColors = ["red", "green", "blue", "yellow", "purple", "orange", "pink"];
 
-createMap(svg,colorScale)    
-defineMapChangeEvent(svg,colorScale)
-defineRunCSPEvent(svg,allColors)
+createMap(svg, colorScale)
+defineMapChangeEvent(svg, colorScale)
+defineRunCSPEvent(svg, allColors)
 checkSearchOptions()
